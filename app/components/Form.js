87e6b62aeaff2dde735d52570b876845da3fc7e5
ishowta <kinironote@gmail.com>
2018-10-25 15:47:00 +0900

    checkboxを追加

--- app/components/Form.js
+++ app/components/Form.js
@@ -11,21 +11,26 @@ export class Form extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { comment: '' };
+    this.state = { comment: '', doTweet: true };
   }
 
   handleChangeComment = (event) => {
     this.setState({ comment: event.target.value });
   }
 
+  handleToggleDoTweetCheckBox = (event) => {
+    this.setState({ doTweet: !this.state.doTweet });
+  }
+
   handleSubmit = async (e) => {
     e.preventDefault();
     const res = await this.props.post(this.state.comment);
     if (res.status === 'SUCCESS') {
+      if (this.state.doTweet) {
+        const twitterTab = window.open(`http://twitter.com/?status=${this.state.comment} / ${`${res.data.entry.title} ` || ''}${hostUrl}/entries/${res.data.entry.id}`, '_blank');
+        twitterTab.focus();
+      }
       this.setState({ comment: '' });
-      console.log(res);
-      const twitterTab = window.open(`http://twitter.com/?status=${res.data.entry.title} ${hostUrl}/entries/${res.data.entry.id}`, '_blank');
-      twitterTab.focus();
     } else if (res.status === 'ERROR') {
       // error
     }
@@ -41,6 +46,8 @@ export class Form extends React.Component {
             <input type="text" value={this.state.comment} onChange={this.handleChangeComment} />
           </label>
           <input type="submit" value="Submit" />
+          <input type="checkbox" checked={this.state.doTweet} onChange={this.handleToggleDoTweetCheckBox} />
+          Twitterに投稿する
         </form>
       </div>
     );
